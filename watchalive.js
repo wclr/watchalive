8d21a46eff3d259c0f9813866d62a0554b8d25f8
Alex Oshchepkov <alex.oschepkov@gmail.com>
2014-09-29 18:06:47 +0600

    Arguments support added

--- watchalive.js
+++ watchalive.js
@@ -1,17 +1,27 @@
 var fs = require('fs'),
-    cwd = process.cwd()
+    path = require('path'),
+    cwd = process.cwd(),
+    argv = require('yargs').argv;
 
-var configFileName = 'watchalive.json'
+
+var configFileName = path.resolve(cwd, argv.config || argv._[0] || 'watchalive.json')
 
 var options = {}
 
-if (fs.existsSync(cwd + '/' + configFileName)){
+if (fs.existsSync(configFileName)){
     try {
-        var fileConfig = JSON.parse(fs.readFileSync(cwd + '/' + configFileName))
+        console.log('Reading configuration file', configFileName)
+        var fileConfig = JSON.parse(fs.readFileSync(configFileName))
         options = fileConfig
     } catch(e){
         console.log('Could parse watchalive configuration file', e)
     }
 }
+
+function takeParamFromArgs(param){
+    options[param] = argv[param] || argv[param[0]] || options[param]
+}
+
+;['base', 'port'].forEach(takeParamFromArgs)
 
 require('./lib/start.js')(options)
