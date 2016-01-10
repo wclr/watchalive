0c6707a77e5e92d540054a41ae359145d7038cbd
Alex <alex.oschepkov@gmail.com>
2016-01-10 23:39:55 +0500

    fixes, formatting

--- start.js
+++ start.js
@@ -1,44 +1,66 @@
 #!/usr/bin/env node
 
 var fs = require('fs'),
-    path = require('path'),
-    argv = require('yargs').argv;
+  path = require('path'),
+  argv = require('yargs').argv;
+
 
 var configPaths = [
-    path.resolve(argv.config || argv._[0] || 'watchalive.json'),
-    path.resolve('watchalive.js'),
-    path.resolve('watchalive.config.js'),
-    path.resolve('package.json')
+  path.resolve(argv.config || argv._[0] || 'watchalive.json'),
+  path.resolve('watchalive.js'),
+  path.resolve('watchalive.config.js'),
+  path.resolve('package.json')
 ]
+
+console.log('Starting WATCHALIVE DEV SERVER...')
 
 var getConfig = function(configPath){
 
-    try {
-        console.log('Reading configuration file', configPath)
-        var config = require(configPath)
-        return /package\.json/.test(configPath) ? config.watchalive : config
-    } catch(e){
-        console.log('Could not parse watchalive configuration file', e)
-    }
+  try {
+    console.log('Reading configuration file', configPath)
+    var config = require(configPath)
+    return /package\.json/.test(configPath) ? config.watchalive : config
+  } catch(e){
+    console.log('Could not parse watchalive configuration file', e)
+  }
 }
 
-
+var camelize = function (str) {
+  return (str && str.toString() || '')
+    .toLowerCase()
+    .replace(/-+(.)?/g, function (match, chr) {
+      return chr ? chr.toUpperCase() : '';
+    });
+}
 var config
 
 configPaths.every(function(configPath){
-    if (fs.existsSync(configPath)){
-        config = getConfig(configPath)
-    }
-    return !config
+  if (fs.existsSync(configPath)){
+    config = getConfig(configPath)
+  }
+  return !config
 })
 
 config = config || {}
 
+//var _arg0 = argv['_'][0]
+//var _arg1 = argv['_'][1]
+//
+//if (parseInt(_arg0)){
+//  config.port = parseInt(_arg0)
+//  if (_arg1){
+//    config.base = _arg1
+//  }
+//} else if (_arg0){
+//  config.base = _arg1
+//}
+
 for (var arg in argv){
-    if (arg !== '_'){
-        config[arg] = argv[arg]
-    }
+  if (arg !== '_'){
+    config[camelize(arg)] = argv[arg]
+  }
 }
+
 
 var App = require('./lib/app.js')
 
